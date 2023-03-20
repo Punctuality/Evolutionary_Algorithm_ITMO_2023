8a47b74713c61741dfb34b14f921e25a92ec896d
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2023-03-21 04:28:58 +0600

    Implemented basic pipeline for Queens task

--- Lab_5/src/main/scala/lab5/Main.scala
+++ Lab_5/src/main/scala/lab5/Main.scala
@@ -1,13 +1,61 @@
 package lab5
 
-import lab5.evoalgo.*
 import cats.implicits.*
+import lab5.eval.CombinedTerminationCondition
+import lab5.evoalgo.*
+import org.uncommons.watchmaker.framework.*
+import org.uncommons.watchmaker.framework.operators.*
+import org.uncommons.watchmaker.framework.selection.*
+import org.uncommons.watchmaker.framework.termination.*
+
+import scala.jdk.CollectionConverters.*
 
 object Main extends App:
-  val s = QueensSolution(Array(1, 0, 3, 2, 5, 4, 7, 6))
-  val ff = QueensFitnessFunction()
+  val dimension = 200
+  val populationSize = 10
+  val generations = 10000
+  val random = java.util.Random()
 
-  println(s"Solution: $s\n(${s.show})")
-  println(s"Sum on 2 column: ${s.intersectionsOn(2)}")
-  println(s"Fitness on example: ${ff.getFitness(s, null)}")
+  val factory = QueensFactory(dimension)
+  val operators = List(
+    QueensCrossover(3, 0.05),
+    QueensMutation(QueensMutation.MutationType.Swap, 0.5)
+  )
+  val pipeline = EvolutionPipeline(operators.asJava)
 
+  val selection = RouletteWheelSelection()
+
+  val evaluator = QueensFitnessFunction()
+
+  val algorithm = SteadyStateEvolutionEngine(
+    factory,
+    pipeline,
+    evaluator,
+    selection,
+    populationSize,
+    false,
+    random
+  )
+
+  val observer: EvolutionObserver[QueensSolution] =
+    (populationData: PopulationData[_ <: QueensSolution]) =>
+      val iter = populationData.getGenerationNumber()
+      if iter % 100 == 0 || iter == generations then
+          val bestFit = populationData.getBestCandidateFitness();
+          System.out.println(s"Generation ${populationData.getGenerationNumber()}: $bestFit");
+          val best: QueensSolution = populationData.getBestCandidate();
+          System.out.println(s"\tBest solution = ${best.show}")
+
+  algorithm.addEvolutionObserver(observer)
+
+  val terminate = CombinedTerminationCondition(
+    CombinedTerminationCondition.CombineType.OR,
+    GenerationCount(generations + 1),
+    TargetFitness(0.0, evaluator.isNatural)
+  )
+
+  val startTime = System.currentTimeMillis()
+  val result = algorithm.evolve(populationSize, 1, terminate)
+  System.out.println(s"Result (${evaluator.getFitness(result, null)}) = ${result.show}")
+  val endTime = System.currentTimeMillis()
+  println(s"Time: ${endTime - startTime} ms")
